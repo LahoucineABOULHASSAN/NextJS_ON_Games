ae8b3bcf545cdd03e3e2b64c4163ba820e9b13a1
lahoucine aboulhassan <em.aboulhassan@gmail.com>
2021-09-04 19:04:20 +0100

    update single game comps

--- pages/games/About.jsx
+++ pages/games/About.jsx
@@ -1,12 +1,14 @@
-import AboutDetails from "./AboutDetails";
-import AboutMoreDetails from "./AboutMoreDetails";
-
-const About = ({ show }) => {
+const About = ({ game }) => {
+  const { title, status, description } = game;
   return (
-    <div className="w-full p-10 bg-gray-900">
-      <AboutDetails show={show} />
-      <AboutMoreDetails show={show} />
-    </div>
+    <>
+      <h1 className="text-2xl text-green-500 font-semibold mb-4">About</h1>
+      <div className="rounded-lg text-sm text-gray-400 bg-gray-700 p-4">
+        <h3 className="text-lg text-gray-300 font-bold uppercase">{title}</h3>
+        <h4 className="mb-4">{status}</h4>
+        <p className="text-justify leading-6">{description}</p>
+      </div>
+    </>
   );
 };
 
--- pages/games/[id].jsx
+++ pages/games/[id].jsx
@@ -1,16 +1,14 @@
-import Seasons from "./Seasons";
-import CastAndCrew from "./CastAndCrew";
 import About from "./About";
 
-const URL = "https://api.tvmaze.com/shows";
+const URL = "https://www.freetogame.com/api/games";
 
 export const getStaticPaths = async () => {
   const res = await fetch(URL);
   const data = await res.json();
 
-  const paths = data.map((show) => {
+  const paths = data.map((game) => {
     return {
-      params: { id: show.id.toString() },
+      params: { id: game.id.toString() },
     };
   });
 
@@ -20,48 +18,21 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context) => {
   const id = context.params.id;
 
-  // fetch single show
-  const showRes = await fetch(`${URL}/${id}`);
-  const show = await showRes.json();
-
-  // fetch seasons
-  const seasonsRes = await fetch(`${URL}/${id}/seasons`);
-  const seasons = await seasonsRes.json();
-
-  // fetch cast
-  const castandcrewRes = await fetch(`${URL}/${id}/cast`);
-  const castandcrew = await castandcrewRes.json();
+  // fetch single game
+  const gameRes = await fetch(`https://www.freetogame.com/api/game?id=${id}`);
+  const game = await gameRes.json();
 
   return {
-    props: { show, seasons, castandcrew },
+    props: { game },
   };
 };
 
-const Serie = ({ show, seasons, castandcrew }) => {
-  const { name, image, rating } = show;
+const Serie = ({ game }) => {
+  const { title } = game;
   return (
     <section>
-      <div className="w-full overflow-hidden show-header relative">
-        <div className="absolute top-0 w-full show-header-title">
-          <h1 className="relative text-6xl font-bold tracking-widest text-gray-400 w-full uppercase w-10/12 py-8 mx-auto">
-            {name}
-            <small className="absolute bottom-0 left-10 text-lg tracking-wide text-green-300">
-              {rating.average} <span className="text-sm">/10</span>
-            </small>
-          </h1>
-        </div>
-        <img
-          height="944"
-          width="1679"
-          className="w-full h-full"
-          src={image.original}
-          alt={name}
-        />
-        <div className="absolute bottom-0 w-full h-full show-header-shadow"></div>
-      </div>
-      <Seasons name={name} seasons={seasons} />
-      <CastAndCrew castandcrew={castandcrew} />
-      <About show={show} />
+      <h1>{title}</h1>
+      <About game={game} />
     </section>
   );
 };
