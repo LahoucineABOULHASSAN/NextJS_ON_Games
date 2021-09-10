6fa3d65bf5fe882cbfaf711f15b9de411ea96c2a
lahoucine aboulhassan <em.aboulhassan@gmail.com>
2021-09-10 15:29:50 +0100

    update game details

--- pages/games/Details.jsx
+++ pages/games/Details.jsx
@@ -36,47 +36,53 @@ const Details = ({ game }) => {
               <span className="text-sm text-gray-400">{game.developer}</span>
             </h3>
           </div>
-          <div className="w-full md:w-auto">
-            <h1 className="text-md text-center md:text-left font-semibold  text-purple-300 tracking-widest  capitalize w-auto mb-3">
-              minimum system requirements
-            </h1>
-            <h3>
-              <span className="text-gray-300 font-semibold mr-4">Memory:</span>{" "}
-              <span className="text-sm text-gray-400">
-                {game.minimum_system_requirements.memory}
-              </span>
-            </h3>
-            <h3>
-              <span className="text-gray-300 font-semibold mr-4">Storage:</span>{" "}
-              <span className="text-sm text-gray-400">
-                {game.minimum_system_requirements.storage}
-              </span>
-            </h3>
-            <h3>
-              <span className="text-gray-300 font-semibold mr-4">
-                Processor:
-              </span>{" "}
-              <span className="text-sm text-gray-400">
-                {game.minimum_system_requirements.processor}
-              </span>
-            </h3>
-            <h3>
-              <span className="text-gray-300 font-semibold mr-4">
-                Operation System:
-              </span>{" "}
-              <span className="text-sm text-gray-400">
-                {game.minimum_system_requirements.os}
-              </span>
-            </h3>
-            <h3>
-              <span className="text-gray-300 font-semibold mr-4">
-                Graphics:
-              </span>{" "}
-              <span className="text-sm text-gray-400">
-                {game.minimum_system_requirements.graphics}
-              </span>
-            </h3>
-          </div>
+          {game.minimum_system_requirements && (
+            <div className="w-full md:w-auto">
+              <h1 className="text-md text-center md:text-left font-semibold  text-purple-300 tracking-widest  capitalize w-auto mb-3">
+                minimum system requirements
+              </h1>
+              <h3>
+                <span className="text-gray-300 font-semibold mr-4">
+                  Memory:
+                </span>{" "}
+                <span className="text-sm text-gray-400">
+                  {game.minimum_system_requirements.memory || "unknown"}
+                </span>
+              </h3>
+              <h3>
+                <span className="text-gray-300 font-semibold mr-4">
+                  Storage:
+                </span>{" "}
+                <span className="text-sm text-gray-400">
+                  {game.minimum_system_requirements.storage || "unknown"}
+                </span>
+              </h3>
+              <h3>
+                <span className="text-gray-300 font-semibold mr-4">
+                  Processor:
+                </span>{" "}
+                <span className="text-sm text-gray-400">
+                  {game.minimum_system_requirements.processor || "unknown"}
+                </span>
+              </h3>
+              <h3>
+                <span className="text-gray-300 font-semibold mr-4">
+                  Operation System:
+                </span>{" "}
+                <span className="text-sm text-gray-400">
+                  {game.minimum_system_requirements.os || "unknown"}
+                </span>
+              </h3>
+              <h3>
+                <span className="text-gray-300 font-semibold mr-4">
+                  Graphics:
+                </span>{" "}
+                <span className="text-sm text-gray-400">
+                  {game.minimum_system_requirements.graphics || "unknown"}
+                </span>
+              </h3>
+            </div>
+          )}
         </div>
       )}
       {game && <Modal game={game} />}
